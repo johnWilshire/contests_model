3c91b827e43ce11276af267103522816ca5a9b4a
John Wilshire <john.h.wilshire@gmail.com>
2016-01-23 14:15:17 +1100

    added R json parser

--- R/load_datasets.R
+++ R/load_datasets.R
@@ -1,4 +1,5 @@
 library(jsonlite)
+library(plyr)
 
 # I am guessing that you are in the top dir
 
@@ -6,13 +7,31 @@ setwd("python/data")
 df <- lapply(
   dir(), 
   function (filename){ 
-    cat(filename)
-    fromJSON(filename)
+    as.data.frame(
+      fromJSON(filename)
+    )
   }
 )
+
+# select these columns for analysis
+master <- data.frame()
+
+# this works but feels pretty gross and not R like
+# will please teach me the R way
+
+for (simulation in df){
+    simulation <- data.frame(
+      simulation$traits.aggression,
+      simulation$parameters.patch_area
+    )
+    colnames(simulation) <- c(
+      "aggression",
+      "patch_area"
+    )
+    master <- rbind(master, simulation)
+}
+
 setwd("../..")
-
-
 
 
 
