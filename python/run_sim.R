617a4a33d078a75b9ec18e354af7884a6fcff720
John Wilshire <john.h.wilshire@gmail.com>
2016-02-13 17:21:03 +1100

    small run_sim bug

--- python/run_sim.R
+++ python/run_sim.R
@@ -1,6 +1,8 @@
 # this script is called from the pbs script
 args <- commandArgs(trailingOnly = TRUE)
-args[2:5] <- as.numeric(args[2:5])
-
 source("../R/run_simulation.R")
-run_simulation(args[1], args[2], args[3], args[4], args[5])
+run_simulation(args[1], 
+    as.numeric(args[2]), 
+    as.numeric(args[3]), 
+    as.numeric(args[4]), 
+    as.numeric(args[5]))
