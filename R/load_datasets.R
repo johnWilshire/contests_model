0260e650c51a0180aa85e7ec6b0f194ea7b0deae
John Wilshire <john.h.wilshire@gmail.com>
2016-02-17 14:47:59 +1100

    lin commitment

--- R/load_datasets.R
+++ R/load_datasets.R
@@ -2,57 +2,28 @@
 # returns a data.frame
 # args:
 # directory: the folder in which the collection of jsons will be read from
-# levels: the number of levels to change directory back to
-# ie python/data -> 2
-load_population  <- function (directory  = "", levels = 0){
-  setwd(directory)
-  df <- lapply(
-    dir()[grep("*.json", dir())], 
-    function (filename){ 
-      as.data.frame(
-        # ignore the trait history portion
-        fromJSON(filename)[1:2]
-      )
-    }
-  )
-  
-  # this doesnt feel like the right way to do this
-  # I cant do a plyr join as they might not have the same number of columns as I modify
-  master <- data.frame()
-  for (simulation in df){
-    simulation <- data.frame(simulation)
-    # rename them
-    colnames(simulation) <- lapply(colnames(simulation), 
-                                  function(x) {sub("^[^.]*.","",  x)})
-    master <- rbind(master, simulation)
-  }
-  setwd(Reduce(function(...) {paste(..., sep = "")}, rep("../", levels)))
+# plyr <3 <3 <3 <3 <3
+
+load_population  <- function (directory  = ""){
+  master <- ldply(list.files(directory, full.names = TRUE),                   
+                  function (filename) {
+                    data.frame(
+                      fromJSON(filename)[1:2]
+                    )})
+  colnames(master) <- sub("^[^.]*.","", colnames(master))
   return(master)
 }
 
 # loads the trait history
 # and energy
 # returns a data.frame
-
-load_trait_history  <- function (directory  = "", levels = 0){
-  setwd(directory)
-  traits <- lapply(
-    dir()[grep("*.json", dir())], 
-    function (filename){ 
-      as.data.frame(
-        # read the trait history portion
-        fromJSON(filename)[2:3]
-      )
-    }
-  )
-  master <- data.frame()
-  for (simulation in traits){
-    simulation <- data.frame(simulation)
-    # rename them
-    colnames(simulation) <- lapply(colnames(simulation), 
-                                   function(x) {sub("^[^.]*.","",  x)})
-    master <- rbind(master, simulation)
-  }
-  setwd(Reduce(function(...) {paste(..., sep = "")}, rep("../", levels)))
+# plyr <3 <3 <3
+load_trait_history  <- function (directory  = ""){
+  master <- ldply(list.files(directory, full.names = TRUE),                   
+                  function (filename) {
+                    data.frame(
+                      fromJSON(filename)[2:3]
+                    )})
+  colnames(master) <- sub("^[^.]*.","", colnames(master))
   return(master)
 }
